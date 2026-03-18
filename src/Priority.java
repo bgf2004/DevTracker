681971c8d98ff15b3e21b713bc5bb8d0a9ea1b9e
Bryan Cortez <BF0414212004@outlook.com>
2026-03-18 17:21:25 -0500

    feat: add priority enum

---
+++ src/Priority.java
@@ -0,0 +1,9 @@
+/*
+This will represent the urgency level of a certain task and they will be ordered from highest to lowest priority.
+This will be used for PriorityQueue
+ */
+public enum Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
