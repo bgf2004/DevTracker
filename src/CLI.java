64b92f032f68a85f3b511452ee8e2dd8afcd0366
Bryan Cortez <BF0414212004@outlook.com>
2026-03-20 01:51:04 -0500

    fix: completeTask method returns bool to stop false success message

--- src/CLI.java
+++ src/CLI.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CLI {
+    public static void main(String[] args) {
+        TaskManager task = new TaskManager();
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Welcome to TaskManager!\n");
+        while(true){
+            System.out.println("Enter a command - add, list, complete, exit: ");
+            String input = scnr.nextLine().trim().toLowerCase();
+
+            if(input.equals("add")){
+                System.out.println("Title of the task: ");
+                String title = scnr.nextLine().trim();
+                System.out.println("Priority (HIGH/MEDIUM/LOW): ");
+                Priority priority = Priority.valueOf(scnr.nextLine().trim().toUpperCase());
+                task.addTask(title, priority);
+                System.out.println("Task added\n");
+
+            } else if (input.equals("list")) {
+                task.listTasks();
+
+            } else if (input.equals("complete")) {
+                System.out.println("Enter ID of task: ");
+                int id = scnr.nextInt();
+                scnr.nextLine();
+                if(task.completeTask(id)) {
+                    System.out.println("Task " + id + " marked as finished\n");
+                }
+
+            } else if (input.equals("exit")) {
+                System.out.println("Goodbye!");
+                return;
+            }else {
+                System.out.println("Invalid command choose from: add, list, complete, exit: ");
+            }
+        }
+    }
 }
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class TaskManager {
+public class TaskManager{
+    private PriorityQueue<Task> tasks = new PriorityQueue<>();
+    private int nextID = 1;
+
+    public void addTask(String title, Priority priority){
+        tasks.add(new Task(nextID, title, priority));
+        nextID++;
+    }
+
+    public boolean completeTask(int id){
+        boolean found = false;
+        for(Task task : tasks){
+            if(task.getTaskID() == id && !task.isDone()){
+                task.markDone();
+                found = true;
+            }
+        }
+        if(!found){
+            System.out.println("There is no task with ID: " + id);
+        }
+        return found;
+    }
+
+    public void listTasks(){
+        List<Task> sortedList = new ArrayList<>(tasks);
+        Collections.sort(sortedList);
+        for(Task task : sortedList){
+            System.out.println(task.toString());
+        }
+    }
+
 }
